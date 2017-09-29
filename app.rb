90d51e608a68459057f4c1adb9db2a5311c4ac97
tylermiller94 <tylermiller94@gmail.com>
2017-09-29 16:41:22 -0700

    add views and brand form

--- app.rb
+++ app.rb
@@ -4,3 +4,16 @@ require 'pry'
 
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get "/" do
+  @stores = Store.all
+  erb(:stores)
+end
+
+#stores
+
+post "/stores" do
+   name = params["name"]
+   store.create({name: name})
+   redirect "/stores"
+end
