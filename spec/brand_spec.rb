1ee4f0e833789998b5ede8498e66c0fb9ca09eab
tylermiller94 <tylermiller94@gmail.com>
2017-09-29 14:36:43 -0700

    add validation method and spec

--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -1,5 +1,9 @@
-require "spec helper"
+require "spec_helper"
 
 describe(Brand) do
-  it{should have_and_belong_to_many(:stores)}
+  it { should have_and_belong_to_many(:stores) }
+  it ("validates a name") do
+    brand = Brand.new({name:""})
+    expect(brand.save).to eq false
+  end
 end
--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -2,4 +2,8 @@ require "spec_helper"
 
 describe(Store) do
   it { should have_and_belong_to_many(:brands)}
+  it ("validates a name") do
+    store = Store.new({name:""})
+    expect(store.save).to eq false
+  end
 end
