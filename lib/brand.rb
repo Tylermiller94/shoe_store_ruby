b920098f72bc29a3c68ffc391010fd77382aed5b
Guest User <Guest@epicodus-41.local>
2017-10-08 00:00:16 -0700

    create currency callback method to brand class

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,10 +1,16 @@
 class Brand < ActiveRecord::Base
   has_and_belongs_to_many(:stores)
   before_save(:upcase_first_letter)
-  validates(:name, {:presence => true})
+  before_save(:currency)
 
+  validates(:name, {:presence => true, :length => {:maximum => 100}})
+  validates(:name, uniqueness: { case_sensitive: false })
 
   define_method(:upcase_first_letter) do
     self.name =  self.name.slice(0,1).capitalize + self.name.slice(1..-1)
+  end
+
+  define_method(:currency) do
+    self.price = ('%.2f' % (self.price.to_f)).prepend("$")
   end
 end
--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -1,4 +1,4 @@
-require "spec_helper"
+require("spec_helper")
 
 describe(Brand) do
   it("has many stores") do
@@ -15,5 +15,20 @@ describe(Brand) do
   it("will ensure that a name is entered") do
     brand1 = Brand.new({:name => "", :price => "50.00"})
     expect(brand1.save).to(eq(false))
+  end
+  it("will ensure the length of the name is at most 100 characters") do
+    brand1 = Brand.new({:name => "qweruyqwoieruyoqwieuyrqwoieuryqowieuyrqowieuryqoweiuryqowieuryqowieuryqowieuryq23iuyroqwieryqoiwueryqwoeiury", :price => "50.00"})
+    expect(brand1.save).to(eq(false))
+  end
+  it("will ensure a new brand name is unique") do
+    brand1 = Brand.new({:name => "nike", :price => "50.00"})
+    brand1.save
+    brand2 = Brand.new({:name => "nike", :price => "50.00"})
+    expect(brand2.save).to(eq(false))
+  end
+  it("will ensure that the price is returned as currency") do
+    brand1 = Brand.new({:name => "nike", :price => "50"})
+    brand1.save
+    expect(brand1.price).to(eq("$50.00"))
   end
 end
